6215778e1acd4af7f6d725c842484cc2b18b74b9
Mazen Morgan <morgan.mazen42@gmail.com>
2018-08-26 02:27:48 +0200

    SettingUp AI pawn and Navigation Setup

---
+++ CoopGame/Source/CoopGame/Private/AI/STrackerBot.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "STrackerBot.h"
+
+#include "Components/StaticMeshComponent.h"
+
+
+// Sets default values
+ASTrackerBot::ASTrackerBot()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh Component"));
+	MeshComp->SetCanEverAffectNavigation(false);
+	RootComponent = MeshComp;
+
+}
+
+// Called when the game starts or when spawned
+void ASTrackerBot::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASTrackerBot::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
---
+++ CoopGame/Source/CoopGame/Public/AI/STrackerBot.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "STrackerBot.generated.h"
+
+UCLASS()
+class COOPGAME_API ASTrackerBot : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ASTrackerBot();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+
+	
+};
